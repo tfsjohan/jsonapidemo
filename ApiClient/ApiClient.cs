cc60464d6e9e3cbc68c4a973f9f81a2201ba89ae
Johan Nordberg <johan@ifokus.se>
2022-11-30 17:03:45 +0100

    First commit

---
+++ ApiClient/ApiClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    using System.Net.Http;
+    
+    public class ApiClient
+    {
+        private static HttpClient _sharedClient;
+
+        public ApiClient(string baseUrl, string authHeader = null)
+        {
+            _sharedClient = new HttpClient()
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+            
+
+            if (!string.IsNullOrEmpty(authHeader))
+            {
+                _sharedClient.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(authHeader);
+            }
+        }
+
+        public async Task<T> GetJson<T>(string path)
+        {
+            return await _sharedClient.GetFromJsonAsync<T>(path, new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+        }
+    }
+}
---
+++ CSharpCoach/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharpCoach
+{
+    public class DiscussionResponse
+    {
+        public int Id { get; set; }
+
+        public int AccessRight { get; set; }
+
+        public Discussion Discussion { get; set; }
+    }
+
+    public class Discussion
+    {
+        public string Title { get; set; }
+    }
+    
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var api = new ApiClient.ApiClient("https://api.ifokus.se");
+
+            var discussion = await api.GetJson<DiscussionResponse>("/api/v1/discussion/1553786");
+
+            if (discussion != null)
+            {
+                Console.WriteLine(discussion.Discussion.Title);
+            }
+
+        }
+    }
+}
